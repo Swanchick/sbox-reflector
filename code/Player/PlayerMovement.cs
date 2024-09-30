28ca41b32dcc82da79a545212f06146ea29263cd
Swanchick <61909965+Swanchick@users.noreply.github.com>
2024-09-30 19:21:50 +0300

    Added some components

---
+++ code/Player/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using Sandbox;
+
+
+public class PlayerMovement : Component
+{
+	protected override void OnStart()
+	{
+		base.OnStart();
+	}
+
+	protected override void OnUpdate()
+	{
+		base.OnUpdate();
+	}
+}
---
+++ code/TestComponent.cs
@@ -0,0 +1,15 @@
+﻿using Sandbox;
+using System;
+
+
+public class TestComponent : Component
+{
+	[Property] private string text;
+
+	protected override void OnStart()
+	{
+		Log.Info( text );
+
+		base.OnStart();
+	}
+}
