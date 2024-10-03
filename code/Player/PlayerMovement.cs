3eb629da1101290fec0c5a8c0c15015b82bd535a
Swanchick <61909965+Swanchick@users.noreply.github.com>
2024-10-04 00:37:19 +0300

    Little update

--- code/Player/PlayerMovement.cs
+++ code/Player/PlayerMovement.cs
@@ -3,13 +3,31 @@
 
 public class PlayerMovement : Component
 {
+	private CharacterController characterController;
+
 	protected override void OnStart()
 	{
+		characterController = Components.Get<CharacterController>();
+
 		base.OnStart();
 	}
 
 	protected override void OnUpdate()
 	{
+		
+
 		base.OnUpdate();
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		Movement();
+
+		base.OnFixedUpdate();
+	}
+
+	private void Movement()
+	{
+		
 	}
 }
