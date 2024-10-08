854dba5ddb23a6f35ded609cdf4b42a6e05002e8
Swanchick <61909965+Swanchick@users.noreply.github.com>
2024-10-08 21:37:07 +0300

    Update

--- code/Item/Item.cs
+++ code/Item/Item.cs
@@ -3,6 +3,8 @@
 	[Property]
 	private GameObject diskThrower;
 
+	public ItemStand ItemStand { get; set; }
+
 	protected override void OnPlayerEnter( Player player )
 	{
 		DiskWeapon diskWeapon = player.DiskWeapon;
@@ -12,8 +14,13 @@
 		if ( !diskWeapon.CanAddThrower() )
 			return;
 
+		if ( ItemStand == null )
+			return;
+
+		ItemStand.OnItemPickup();
+
 		diskWeapon.AddThrower( diskThrower );
-		
+
 		GameObject.Destroy();
 	}
 }
---
+++ code/Item/ItemStand.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+
+public class ItemStand : Component
+{
+	[Property]
+	private List<GameObject> Items;
+
+	[Property]
+	private int respawnTime = 5000;
+
+	[Property]
+	private GameObject itemSpace;
+
+	public void OnItemPickup()
+	{
+		_ = WaitAndSpawn();
+	}
+
+	protected override void OnStart()
+	{
+		SpawnRandomItem();
+	}
+
+	private async Task WaitAndSpawn()
+	{
+		await Task.Delay(respawnTime);
+
+		SpawnRandomItem();
+	}
+
+	private void SpawnRandomItem()
+	{
+		GameObject itemObject = Items[Game.Random.Next( 0, Items.Count - 1 )];
+		if ( itemObject == null )
+			return;
+
+		GameObject itemObjectNew = itemObject.Clone( itemSpace, Vector3.Zero, Rotation.Identity, Vector3.One );
+		itemObjectNew.NetworkSpawn();
+
+		Item item = itemObjectNew.GetComponent<Item>();
+		if ( item == null )
+			return;
+
+		item.ItemStand = this;
+	}
+}
